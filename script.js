8466898392952713bd055d061a35a59efb06da83
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2023-03-20 22:43:35 +0800

    ДЗ1

---
+++ script.js
@@ -0,0 +1,10 @@
+const celsius = 35;
+const fahrenheit = (9/5 * celsius) +32;
+
+console.log(celsius, '°C');
+console.log(fahrenheit, '°F');
+
+const name = 'Екатерина';
+const admin = name;
+
+console.log(admin);
